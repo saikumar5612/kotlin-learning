75651f0c51b4067c6e66fcf2db9b101833029053
saikumar atchi <saikumaratchi@gmail.com>
2025-01-20 17:36:04 -0500

    second commit with varible concept

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var userOne:String = "Alex"
+    userOne = "Jhon"
+    var userOneAge:Int = 22
+    var userTwoAge:Int = 23
+
+    val userTwo:String = "Michel"
+    println("Hello user one, $userOne!, your age is $userOneAge")
+    println("Hello user two, $userTwo!, your age is $userTwoAge")
+}
