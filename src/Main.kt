7a7d574454e9cb196eb045c86237899a67b8b060
saikumar atchi <saikumaratchi@gmail.com>
2025-01-20 18:48:25 -0500

    commit with int short long byte data types

---
+++ src/Main.kt
@@ -0,0 +1,29 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    var number:Int = 22
+    val maxIntegerValue:Int = Int.MAX_VALUE
+    val minIntegerValue:Int = Int.MIN_VALUE
+
+     println("number is: $number")
+    println("Int maximum value is: $maxIntegerValue")
+    println("Int minimum value is: $minIntegerValue")
+
+    var myMaxByteValue:Byte = Byte.MAX_VALUE
+    var myMinByteValue:Byte = Byte.MIN_VALUE
+    println("My Byte max value is: $myMaxByteValue")
+    println("MY Byte min value is: $myMinByteValue")
+
+    var myShortMaxValue:Short= Short.MAX_VALUE
+    var myShortMinValue:Short= Short.MIN_VALUE
+    println("my short max value is: $myShortMaxValue")
+    println("my short min value is: $myShortMinValue")
+
+    var myLongMaxValue:Long= Long.MAX_VALUE
+    var myLongMinValue:Long=Long.MIN_VALUE
+
+    println("my Long max value is: $myLongMaxValue")
+    println("my long min value is: $myLongMinValue")
+
+}
