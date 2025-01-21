018b5382799c38031aa2c4a62d52f87abda98cba
saikumar atchi <saikumaratchi@gmail.com>
2025-01-20 19:17:03 -0500

    commit with float and double data types usage

---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+
+    val myFloatMaxNumber:Float = Float.MAX_VALUE
+    val myFloatMinNumber:Float = Float.MIN_VALUE
+    val myDoubleMaxValue:Double=Double.MAX_VALUE
+    val myDoubleMinValue:Double=Double.MIN_VALUE
+
+    var myFloatNumber = 2.5f
+
+
+    println("my Float Max Number is: $myFloatMaxNumber")
+    println("my float  number is: $myFloatNumber")
+}
